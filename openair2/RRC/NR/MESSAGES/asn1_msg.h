761a692c1cc4d34f26617673c4a2f84e1d10e568
Imran Khan <imranbuet63@gmail.com>
2024-04-02 20:09:58 -0400

    using meas_config clone

--- openair2/RRC/NR/MESSAGES/asn1_msg.h
+++ openair2/RRC/NR/MESSAGES/asn1_msg.h
@@ -150,7 +150,7 @@ int do_RRCReestablishment(rrc_gNB_ue_context_t *const ue_context_pP,
 
 int do_RRCReestablishmentComplete(uint8_t *buffer, size_t buffer_size, int64_t rrc_TransactionIdentifier);
 
-NR_MeasConfig_t *get_defaultMeasConfig(uint32_t absFreqSSB, int band, int scs, const module_id_t module_id);
+NR_MeasConfig_t *get_defaultMeasConfig(uint32_t absFreqSSB, int band, int scs);
 NR_MeasConfig_t *get_MeasConfig(uint32_t ssb_arfcn,
                                 int band,
                                 int scs,
